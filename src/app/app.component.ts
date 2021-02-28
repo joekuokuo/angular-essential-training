c948f4ebf96c8c19292e98d2a6be4034d97f71dc
Tsung-Da Kuo <tsungdakuo@gmail.com>
2021-02-27 19:23:36 -0800

    create css and html for app component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "mw-app",
-  template: "<h1>My app</h1>",
+  // template: "<h1>My app</h1>",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {}
