c52502607f90effe60846e97d78f2cd3202b9996
Tsung-Da Kuo <tsungdakuo@gmail.com>
2021-03-01 18:40:29 -0800

    Add favorite directive and use it in the media-item component

--- src/app/favorite.directives.ts
+++ src/app/favorite.directives.ts
@@ -1,8 +1,16 @@
-import { Directive, HostBinding } from "@angular/core";
+import { Directive, HostBinding, Input } from "@angular/core";
 
 @Directive({
   selector: "[mwFavorite]",
 })
 export class FavortieDirective {
   @HostBinding("class.is-favorite") isFavorite = true;
+  // use input decorator to decorate a class setter method
+  // a setter method is called when a property with the same name
+  // the setter method will receive a value
+
+  @Input() set mwFavorite(value) {
+    //   value is a boolean
+    this.isFavorite = value;
+  }
 }
