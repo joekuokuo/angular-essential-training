a3b44dda296970b4546e0d16e7b0fd05d9eb1cda
Tsung-Da Kuo <tsungdakuo@gmail.com>
2021-03-03 20:36:40 -0800

    Add FormsModule in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+
 import { AppComponent } from "./app.component";
 import { MediaItemComponent } from "./media-item.component";
 import { MediaItemListComponent } from "./media-item-list.component";
@@ -7,7 +9,7 @@ import { FavortieDirective } from "./favorite.directives";
 import { CategoryListPipe } from "./category-list.pipe";
 
 @NgModule({
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   declarations: [
     AppComponent,
     MediaItemComponent,
